a787151b0029acb897838138289d92a5b49da323
Jimmy <jimmyfo1307@yahoo.fr>
2019-12-18 10:03:12 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { Button, Textfield } from 'react-mdl';
+import bck from './homeBackground.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+      <body  className="App-body" style={{ display: 'flex', backgroundImage: `url(${bck})`, backgroundSize: 'cover' }}>
+        <img src={logo} className="App-logo" alt="logo" /> 
+
+        <Textfield
+          onChange={() => { }}
+          label="Email"
+          floatingLabel
+          style={{ width: '200px' }}
+        />
+   
+        <Textfield
+          onChange={() => { }}                    
+          label="Mot de passe"
+          floatingLabel
+          style={{ width: '200px' }}  
+        />
+        <Button  style={{ backgroundColor: 'white' }}>
+          Se connecter
+        </Button>
+      </body>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>                
+        <Switch>
+          <Route exact path="/" component={App} />        
+          {/* <Route component={Notfound} /> */}
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    </Router>
+  );
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
