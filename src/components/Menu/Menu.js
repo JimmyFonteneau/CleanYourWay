502275264d0638b4d77f110768695e70d7c5174b
Jimmy <jimmyfo1307@yahoo.fr>
2019-12-18 14:42:08 +0100

    Close menu après le click

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,27 +5,34 @@ import 'react-mdl/extra/material.js';
 import {Content, Drawer, Header, Layout, Navigation} from "react-mdl";
 import { Link } from "react-router-dom";
 
-function Menu(props) {
-  return (
 
-      <div style={{height: '100vh', position: 'relative'}}>
-          <Layout fixedHeader>
-              <Header title={<strong>The Title</strong>}>
 
-              </Header>
+class Menu extends React.Component {
+  
+    hideToggle() {
+        var selectorId = document.querySelector('.mdl-layout');
+        selectorId.MaterialLayout.toggleDrawer();
+    }
 
-              <Drawer title="Title">
-                  <Navigation>
-                  <Link to="/savings">Mes économies</Link>
-                  </Navigation>
-              </Drawer>
+    render() {
+        return (
 
-              <Content>
-                  {props.children}
-              </Content>
-          </Layout>
-      </div>
-  );
+            <div style={{height: '100vh', position: 'relative'}}>
+                <Layout fixedDrawer>            
+                    <Drawer title="Title">
+                        <Navigation>
+                        <Link to="/savings" onClick={() => this.hideToggle()}>Mes économies</Link>
+                        </Navigation>
+                    </Drawer>
+      
+                    <Content>
+                        {this.props.children}
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
+   
 }
 
 export default Menu;
