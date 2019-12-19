c2f6dd0e09c4ef47e494f0b10473777f919326fe
Jimmy <jimmyfo1307@yahoo.fr>
2019-12-19 14:01:10 +0100

    Structure page account

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,13 +5,27 @@ import 'react-mdl/extra/material.js';
 
 function Account() {
   return (   
-    <div className="Account" style={{ display: 'flex', paddingTop: '50px', justifyContent: 'center' }}>
-      ACCOUNT
+    <div className="Account" style={{ display: 'flex', paddingTop: '50px', flexDirection: 'column' }}>      
        <div>
+         Photo
+       </div>
+       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+         PROFIL 
+       </div>
+       <div style={{ display: 'flex', justifyContent: 'space-around' }}>
          <div> Bulle 1 </div>
          <div> Bulle 2 </div>
          <div> Bulle 3 </div>
-      </div>      
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+         Choisissez votre moyen de transport 
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+        <div> Transport 1 </div>
+        <div> Transport 2 </div>
+        <div> Transport 3 </div>
+        <div> Transport 4 </div>
+      </div>     
     </div>   
   );
 }
