44548477b60c0b541f7b7d9acc41ee0d1af52412
nicolas <joussetn49@gmail.com>
2019-12-19 19:14:08 +0100

    package.json API URL

--- src/services/UsersService.js
+++ src/services/UsersService.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3004/',
+    baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:3004/" : "https://3004-dot-7073245-dot-devshell.appspot.com/",
     timeout: 1000,
 });
 
