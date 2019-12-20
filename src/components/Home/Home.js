739552e092a06ac374010024b620c0c738b98426
Jimmy <jimmyfo1307@yahoo.fr>
2019-12-20 10:41:15 +0100

    Home page log

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,19 +8,20 @@ import './home.css';
 import logo from '../../../src/logo.svg';
 import bck from '../../../src/homeBackground.jpg';
 import { BrowserRouter as Router, Link } from "react-router-dom";
-
+import cywl from '../../../src/cywl.png';
 
 class Home extends React.Component {
   render () {
     return (
  
       <div  className="App-body" style={{ display: 'flex', background: `url(${bck})`, backgroundSize: 'cover' }}>
-        <img src={logo} className="App-logo" alt="logo" />        
-        <Button  style={{ backgroundColor: 'white' }}      
+        <div style={{ display: 'flex', flexDirection: 'column', paddingTop: '20vh' }}>
+        <img src={cywl} className="App-logo" alt="logo" />        
+        <Button  style={{ backgroundColor: 'white', marginTop: '15vh' }}      
         >         
-          <Link to="/login">Login</Link>
+          <Link to="/login">Se connecter</Link>
         </Button>
-        
+        </div>  
       </div>   
     );
   }
